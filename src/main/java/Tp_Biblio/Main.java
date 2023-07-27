45554ca166719b005feb5f07004b036a803b7a33
lasse0203 <138567698+lasse0203@users.noreply.github.com>
2023-07-27 16:42:29 +0200

    Update Main.java
    changemet livre

--- src/main/java/Tp_Biblio/Main.java
+++ src/main/java/Tp_Biblio/Main.java
@@ -6,10 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Bibliotheque bibliotheque = new Bibliotheque();
 
-            Livre.add(new Livre("hello world", "me", 145, false));
-            Livre.add(new Livre(" Me again", "me", 110, false));
-            Livre.add(new Livre("Black", "me", 11, true));
-            Livre.add(new Livre("Black", "me", 11, true));
+        bibliotheque.ajouterLivre(new Livre("hello world", "me", 145, false));
+        bibliotheque.ajouterLivre(new Livre(" Me again", "me", 110, false));
+        bibliotheque.ajouterLivre(new Livre("Black", "me", 11, true));
+        bibliotheque.ajouterLivre(new Livre("Black", "me", 11, true));
+
+        bibliotheque.afficherListeLivres();
 
 
 
